c1e1a40f7077c4e3680f65a3166e9ba1e637b7e3
Samuel Thibault <samuel.thibault@polymtl.ca>
2024-09-04 19:57:04 -0400

    TP1 complété

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+pi = math.pi
+print('L\'aire du cercle est : ', pi * rayon ** 2)
+
+
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Thbault, Samuel")
